c334f02d871b4ebdb06fc9b5d913231bf0826760
callistacaca <541221062@student.smktelkom-pwt.sch.id>
2023-11-28 20:34:05 +0700

    first commit

---
+++ js/words.js
@@ -0,0 +1,26 @@
+let wordList = [
+    {
+        word: "python",
+        hint: "bahasa pemrograman"
+    },
+    {
+        word: "gitar",
+        hint: "instrumen Musik"
+    },
+    {
+        word: "herbivora",
+        hint: "hewan pemakan tumbuhan"
+    },
+    {
+        word: "bumi",
+        hint: "planet tata surya kita"
+    },
+    {
+        word: "katak",
+        hint: "hewan yang hidup di 2 alam"
+    },
+    {
+        word: "oddo",
+        hint: "oppo jika dibalik menjadi"
+    },
+]
